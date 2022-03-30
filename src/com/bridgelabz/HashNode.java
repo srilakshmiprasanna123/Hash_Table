1ac77b794e1007e8122bd6c428b2010b78534de1
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-30 23:36:40 +0530

    first commit

---
+++ src/com/bridgelabz/HashNode.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class HashNode<K,V> {
+    K key;
+    V value;
+    HashNode<K, V> next;
+
+    HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
